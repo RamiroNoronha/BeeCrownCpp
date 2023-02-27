831735ab7da72be0fb33ed39ce21f703a7d043bd
Ramiro Noronha <ramironrribeiro@gmail.com>
2023-02-27 20:27:30 -0300

    FatorialDeNovo

---
+++ FatorialDeNovo/src/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <math.h>
+int size(int n){
+    int s = 0;
+    for(int i = 1; ; i*=10){
+        if(n/i > 0){
+            s++;
+        } else {
+
+            return s;
+        }
+    }
+    
+}
+
+int factorial(int f){
+    if(f == 0){
+        return 1;
+    }
+    if(f == 1){
+        return 1;
+    }
+    int factorialValue = 1;
+    for(int i = 2; i <= f; i++){
+        factorialValue*=i;
+    }
+
+    return factorialValue;
+}
+
+int digitDecimalValue(int n, int f){
+
+    return n * factorial(f);
+}
+
+int separatedDigit(int n, int p, int valueBefore){
+    int divisor = std::pow(10, p );
+    int value = n - valueBefore;
+    int digit = (value % divisor) / std::pow(10, p-1);
+
+    return digit;
+}
+
+
+
+int main() {
+    int n;
+    while(std::cin >> n){
+        if(n == 0){
+            break;
+        }
+        int s = size(n);
+        int valueBefore =0;
+        int decimal = 0;
+        int currentDigit;
+        for(int i =1; i <=s; i++){
+            currentDigit=separatedDigit(n, i, valueBefore);
+            valueBefore+= currentDigit * std::pow(1, i);
+            decimal+=digitDecimalValue(currentDigit, i);
+        }
+        std::cout << decimal << std::endl;
+    }
+}
